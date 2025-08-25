e99c5dca7f607af7a2f5bd6ebac852faff3322ad
KaykeZebendo <zebendokayke@gmail.com>
2025-08-25 18:59:37 +0000

    Adição de mais variáveis e a conclusão do desafio nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,24 +2,30 @@
 
 int main() {
 
-    printf("===Desafio Super Trunfo- Nível Novato===\n");
+    printf("===Desafio Super Trunfo===\n");
 
 
     // Carta 1
     char estado1;
     char codigo1[4];
+    char nomeCidade1 [50];
     int populacao1;
     float area1;
     float pib1;
     int pontosTuristicos1;
+    float pibPerCapita1;
+    float densidade1;
 
     // Carta 2
     char estado2;
     char codigo2[4];
+    char nomeCidade2 [50];
     int populacao2;
     float area2;
     float pib2;
     int pontosTuristicos2;
+    float pibPerCapita2;
+    float densidade2;
 
     // Cadastro da Carta 1
     printf("=== Cadastro da Carta 1 ===\n");
@@ -28,6 +34,9 @@ int main() {
 
     printf("Codigo (ex: A01): ");
     scanf("%s", codigo1);
+    
+    printf("Nome da Cidade: ");
+    scanf(" %49[^\n]", nomeCidade1);
 
     printf("Populacao: ");
     scanf("%d", &populacao1);
@@ -41,6 +50,18 @@ int main() {
     printf("Numero de Pontos Turisticos: ");
     scanf("%d", &pontosTuristicos1);
 
+
+
+// Cálculos carta 1
+
+    // Densidade populacional = população / área
+    densidade1 = populacao1 / area1;
+
+    // PIB per capita = PIB total (convertido para reais) / população
+    pibPerCapita1 = (pib1 * 1000000000) / populacao1; //Convertendo PIB de Bilhões para Reais
+
+
+
     // Cadastro da Carta 2
     printf("\n=== Cadastro da Carta 2 ===\n");
     printf("Estado (A-H): ");
@@ -48,6 +69,9 @@ int main() {
 
     printf("Codigo (ex: B02): ");
     scanf("%s", codigo2);
+    
+    printf("Nome da Cidade: ");
+    scanf(" %49[^\n]", nomeCidade2);
 
     printf("Populacao: ");
     scanf("%d", &populacao2);
@@ -61,6 +85,18 @@ int main() {
     printf("Numero de Pontos Turisticos: ");
     scanf("%d", &pontosTuristicos2);
 
+
+
+    // Cálculos carta 2
+
+    // Densidade populacional = população / área
+    densidade2 = populacao2 / area2;
+
+    // PIB per capita = PIB total (convertido para reais) / população
+    pibPerCapita2 = (pib2 * 1000000000) / populacao2; //Convertendo PIB de Bilhões para Reais
+
+
+
     // Exibindo as cartas
 
     printf("\n===Exibição das Cartas===\n");
@@ -72,6 +108,8 @@ int main() {
 
     printf("Codigo: %s\n", codigo1);
 
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+
     printf("Populacao: %d\n", populacao1);
 
     printf("Area: %.2f km²\n", area1);
@@ -80,12 +118,18 @@ int main() {
 
     printf("Pontos Turisticos: %d\n", pontosTuristicos1);
 
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+
+    printf("PIB per Capita: %.2f reais por habitante\n", pibPerCapita1);
+
     // Carta 2
     printf("\n=== Carta 2 ===\n");
 
     printf("Estado: %c\n", estado2);
 
     printf("Codigo: %s\n", codigo2);
+
+    printf("Nome da Cidade: %s\n", nomeCidade2);
 
     printf("Populacao: %d\n", populacao2);
 
@@ -94,6 +138,10 @@ int main() {
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     
     printf("Pontos Turisticos: %d\n", pontosTuristicos2);
+
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+
+    printf("PIB per Capita: %.2f reais por habitante\n", pibPerCapita2);
 
     return 0;
 
